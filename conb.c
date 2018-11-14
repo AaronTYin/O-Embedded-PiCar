cf9e2a3d45145bb6ef9c3a66af5a3eaa4ecee242
AaronTYin <1355137560@qq.com>
2018-11-14 10:36:17 +0000

    Update conb.c

--- conb.c
+++ conb.c
@@ -2,31 +2,23 @@
 #include <stdlib.h>
 #include <softPwm.h>
 #include <unistd.h>
-#include <errno.h>
-#include <string.h>
 #include <sys/types.h>
 #include <wiringPi.h>
-#define Trig	28
-#define Echo	29
+
 #define sp 500
 
-void ultraInit(void)
-{
-  pinMode(Echo, INPUT);
-  pinMode(Trig, OUTPUT);
-}
 void run()     // 前进
 {
-    	softPwmWrite(4,0); //左轮前进
-	softPwmWrite(1,sp); 
-	softPwmWrite(6,0); //右轮前进
-	softPwmWrite(5,sp);
+    	softPwmWrite(1,sp); //左轮前进
+	softPwmWrite(4,0); 
+	softPwmWrite(5,sp); //右轮前进
+	softPwmWrite(6,0);
 }
 void brake()         //刹车，停车
 {
     	softPwmWrite(1,0); //左轮stop
 	softPwmWrite(4,0); 
-	softPwmWrite(5,0); //stop
+	softPwmWrite(5,0); //右轮stop
 	softPwmWrite(6,0); 
 }
 void right()
@@ -34,21 +26,21 @@ void right()
     	softPwmWrite(1,sp);
 	softPwmWrite(4,0); 
 	softPwmWrite(5,0);
-	softPwmWrite(6,0); 
+	softPwmWrite(6,sp); 
 }
 void left()
 {
     	softPwmWrite(1,0);
-	softPwmWrite(4,0); 
+	softPwmWrite(4,sp); 
 	softPwmWrite(5,sp);
 	softPwmWrite(6,0);  
 }
 void back()          //后退
 {
-    	softPwmWrite(4,sp); //左轮back
-	softPwmWrite(1,0); 
-	softPwmWrite(6,sp); //右轮back
-  	softPwmWrite(5,0); 
+    	softPwmWrite(1,0); //左轮back
+	softPwmWrite(4,sp); 
+	softPwmWrite(5,0); //右轮back
+  	softPwmWrite(6,sp); 
 }
 char getch()
 {
@@ -71,10 +63,10 @@ int main(int argc, char *argv[])
     pinMode (4, OUTPUT);	//IN2
     pinMode (5, OUTPUT);	//IN3
     pinMode (6, OUTPUT);	//IN4
-    softPwmCreate(1,1,250);   
-    softPwmCreate(4,1,250);
-    softPwmCreate(5,1,250);
-    softPwmCreate(6,1,250);
+    softPwmCreate(1,1,500);   
+    softPwmCreate(4,1,500);
+    softPwmCreate(5,1,500);
+    softPwmCreate(6,1,500);
     while(1){
 	key=getch();
 	printf("Key=%d\n",key);
@@ -83,9 +75,9 @@ int main(int argc, char *argv[])
 	else if(key==66)
 	    back();
 	else if(key==67)
-	    right();
-	else if(key==68)
 	    left();
+	else if(key==68)
+	    right();
 	else if(key==32)
 	    brake();
     }
